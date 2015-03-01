113cd6290b421b855f3115149691c32c4cfd57b1
Scott Bollinger <bollsc@gmail.com>
2015-03-01 15:05:36 -0500

    Added an incident report form activity with enough fields to make it usable as there is A LOT of fields on the actual form.

---
+++ OfficerSupport/src/forms/Incident_Report.java
@@ -0,0 +1,39 @@
+package forms;
+
+import com.pss.wsu.officersupport.R;
+import com.pss.wsu.officersupport.R.id;
+import com.pss.wsu.officersupport.R.layout;
+import com.pss.wsu.officersupport.R.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Incident_Report extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_incident__report);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.incident__report, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
